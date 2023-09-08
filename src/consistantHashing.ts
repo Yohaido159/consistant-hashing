dc2a8b488cf5bc952359ab5b295e48972535a445
Yohai Ido <yohai@networx.com>
2023-09-08 15:36:50 +0300

    finish

--- src/consistantHashing.ts
+++ src/consistantHashing.ts
@@ -1,5 +1,10 @@
 import { createHash } from 'crypto';
 
+// @ts-ignore
+Map.prototype.toJSON = function () {
+  return JSON.parse(JSON.stringify(Object.fromEntries(this)));
+};
+
 abstract class IConsistantHashing {
   abstract hashFunction(key: string): number;
   abstract addServer(server: string): void;
@@ -8,51 +13,125 @@ abstract class IConsistantHashing {
   abstract getServers(): string[];
 }
 
-export class ConsistantHashing implements IConsistantHashing {
-  public servers: string[];
-
+export class Logger {
+  public active: boolean;
   constructor() {
-    this.servers = [];
+    this.active = false;
   }
 
-  hashFunction: IConsistantHashing['hashFunction'] = (key) => {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      const char = key.charCodeAt(i);
-      hash = (hash << 5) - hash + char;
-      hash |= 0;
+  setLoggerActive() {
+    this.active = true;
+  }
+
+  setLoggerInactive() {
+    this.active = false;
+  }
+
+  log(...message: any[]) {
+    if (this.active) {
+      console.log(message);
     }
-    return hash;
-  };
+  }
+}
+
+export class ConsistantHashing implements IConsistantHashing {
+  public logger: Logger;
+  public servers: string[] = [];
+  public ring: Map<number, string> = new Map();
+
+  constructor({ logger }: { logger: Logger }) {
+    this.logger = logger;
+    this.placeServersEqually();
+  }
+
+  hashFunction(key: string) {
+    this.logger.log(`hashFunction: Called with key: ${key}`);
+    const hash = createHash('md5').update(key).digest('hex');
+    const hashNumber = parseInt(hash, 16);
+
+    return hashNumber % 360;
+  }
+
+  private placeServersEqually(): void {
+    this.logger.log(`placeServersEqually: Called`);
+    const step = 360 / this.servers.length;
+    this.ring = new Map();
+    this.servers.forEach((server, index) => {
+      const angle = Math.floor(index * step);
+      this.ring.set(angle, server);
+    });
+    this.logger.log(`placeServersEqually finished`);
+    //@ts-ignore
+    this.logger.log(`ring: ${JSON.stringify(this.ring)}`);
+  }
 
   addServer(server: string) {
+    this.logger.log(`addServer: Called with server: ${server}`);
     this.servers.push(server);
-    this.servers.sort();
+    const beforeRing = new Map(this.ring);
+    this.placeServersEqually();
+    this.findAffectedHashSlots(beforeRing);
+    this.logger.log(`addServer finished`);
   }
 
   removeServer(server: string) {
+    this.logger.log(`removeServer: Called with server: ${server}`);
     const index = this.servers.indexOf(server);
     if (index !== -1) {
       this.servers.splice(index, 1);
+      const beforeRing = new Map(this.ring);
+      this.placeServersEqually();
+      this.findAffectedHashSlots(beforeRing);
     }
+    this.logger.log(`index: ${index}`);
+    this.logger.log(`removeServer: ${server}`);
+    this.logger.log(`servers: ${this.servers}`);
+    this.logger.log(`removeServer finished`);
   }
 
-  getServer(key: string) {
+  public getServer(key: string): string | null {
+    this.logger.log(`getServer: Called with key: ${key}`);
     if (this.servers.length === 0) {
       return null;
     }
 
     const hash = this.hashFunction(key);
-    for (let i = 0; i < this.servers.length; i++) {
-      const serverHash = this.hashFunction(this.servers[i]);
-      if (serverHash >= hash) {
-        return this.servers[i];
+    let angle = hash % 360;
+
+    this.logger.log(`hash: ${hash}`);
+    this.logger.log(`angle: ${angle}`);
+
+    for (let i = 0; i < 360; i++) {
+      if (this.ring.has(angle)) {
+        this.logger.log(`ring return: ${this.ring.get(angle)}`);
+        this.logger.log(`getServer finished insde if`);
+        return this.ring.get(angle) as string;
       }
+      angle = (angle + 1) % 360;
+      this.logger.log(`ring not returned angle: ${angle}`);
+      this.logger.log(`getServer finished`);
     }
-    return this.servers[0];
+    return null;
   }
 
   getServers() {
+    this.logger.log(`getServers: Called`);
     return this.servers;
+  }
+
+  public findAffectedHashSlots(ring: Map<number, string>) {
+    this.logger.log(`findAffectedHashSlots: Called`);
+    const afterRing = new Map(this.ring);
+
+    const affectedHashSlots: number[] = [];
+    for (const [angle, server] of ring) {
+      if (afterRing.get(angle) !== server) {
+        affectedHashSlots.push(angle);
+      }
+    }
+
+    this.logger.log(`affectedHashSlots: ${affectedHashSlots}`);
+
+    return affectedHashSlots;
   }
 }
--- tests/consistantHashing.test.ts
+++ tests/consistantHashing.test.ts
@@ -1,16 +1,102 @@
-import { ConsistantHashing } from '../src/consistantHashing';
+//@ts-nocheck
+import { ConsistantHashing, Logger } from '../src/consistantHashing';
 
 describe('Consistant Hashing', () => {
-    let ch: ConsistantHashing;
-    beforeEach(() => {
-        ch = new ConsistantHashing();
-    }
-    );
+  let ch: ConsistantHashing;
+  let logger: Logger;
+  beforeEach(() => {
+    logger = new Logger();
+    ch = new ConsistantHashing({
+      logger,
+    });
+  });
 
-    it('should add server', () => {
-        ch.addServer('server1');
-        expect(ch.servers).toEqual(['server1']);
-    }
+  it('should add server', () => {
+    ch.addServer('server1');
+    expect(ch.servers).toEqual(['server1']);
+  });
+
+  it('should remove server', () => {
+    ch.addServer('server1');
+    ch.removeServer('server1');
+    expect(ch.servers).toEqual([]);
+  });
+
+  it('should get server', () => {
+    // logger.setLoggerActive();
+    ch.addServer('server1');
+    ch.addServer('server2');
+    ch.addServer('server3');
+
+    expect(ch.getServer('user:1')).toEqual('server1');
+  });
+
+  it('should place servers equally', () => {
+    // logger.setLoggerActive();
+
+    ch.addServer('server1');
+    expect(ch.ring.toJSON()).toEqual({ '0': 'server1' });
+
+    ch.addServer('server2');
+    expect(ch.ring.toJSON()).toEqual({ '0': 'server1', '180': 'server2' });
+
+    ch.addServer('server3');
+    expect(ch.ring.toJSON()).toEqual({
+      '0': 'server1',
+      '120': 'server2',
+      '240': 'server3',
+    });
+
+    ch.addServer('server4');
+    expect(ch.ring.toJSON()).toEqual({
+      '0': 'server1',
+      '90': 'server2',
+      '180': 'server3',
+      '270': 'server4',
+    });
+
+    ch.addServer('server5');
+    expect(ch.ring.toJSON()).toEqual({
+      '0': 'server1',
+      '72': 'server2',
+      '144': 'server3',
+      '216': 'server4',
+      '288': 'server5',
+    });
+
+    expect(ch.servers).toEqual(['server1', 'server2', 'server3', 'server4', 'server5']);
+  });
+
+  it('assign keys to servers', () => {
+    // logger.setLoggerActive();
+
+    ch.addServer('server1');
+    ch.addServer('server2');
+    ch.addServer('server3');
+    ch.addServer('server4');
+
+    const serversRing = {
+      '0': 'server1',
+      '90': 'server2',
+      '180': 'server3',
+      '270': 'server4',
+    };
+
+    expect(ch.ring.toJSON()).toEqual(serversRing);
+
+    const hashUser1 = 272;
+    const hashUser2 = 72;
+    const hashUser3 = 352;
+    const hashUser4 = 96;
+
+    expect(ch.hashFunction('user:1')).toEqual(hashUser1);
+    expect(ch.hashFunction('user:2')).toEqual(hashUser2);
+    expect(ch.hashFunction('user:3')).toEqual(hashUser3);
+    expect(ch.hashFunction('user:4')).toEqual(hashUser4);
+
+    expect(ch.getServer('user:1')).toEqual(serversRing[0]);
+    expect(ch.getServer('user:2')).toEqual(serversRing[90]);
+    expect(ch.getServer('user:3')).toEqual(serversRing[0]);
+    expect(ch.getServer('user:4')).toEqual(serversRing[180]);
+  });
 });
-
-        
